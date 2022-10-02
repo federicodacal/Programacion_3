3df62e4da6021c617de82e911324d390f4814059
Federico Dacal <federicodacal@gmail.com>
2022-10-01 21:33:05 -0300

    ejer_registro

--- Clase_03/ejercicios/20_ejercicio/registro.php
+++ Clase_03/ejercicios/20_ejercicio/registro.php
@@ -2,7 +2,7 @@
 /*
 Archivo: registro.php
 método:POST
-Recibe los datos del usuario(nombre, clave,mail )por POST ,
+Recibe los datos del usuario(nombre, clave, mail) por POST ,
 crear un objeto y utilizar sus métodos para poder hacer el alta,
 guardando los datos en usuarios.csv.
 retorna si se pudo agregar o no.
