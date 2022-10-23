73ce87a2485bada190630a3e9c2c05aa30421e65
Federico Dacal <federicodacal@gmail.com>
2022-10-23 18:24:47 -0300

    clase_api_slim

---
+++ Clase_07_ApiRest/public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../src/app/app.php'; // referencio el index de public a otra pagina
+
+/*
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello World! SlimFramework");
+    return $response;
+});
+
+$app->run();
+*/
+
+?>
